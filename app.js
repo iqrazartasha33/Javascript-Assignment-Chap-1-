cf629d50caee70b4521b7d8e6f361e180330c3e5
iqrazartasha33 <iqrazartasha33@gmail.com>
2024-09-30 21:23:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+//Chap 1 (Alert Chapter)
+
+
+// Ans 1 the function show message in pops
+
+// Ans 2
+alert("Welcome to my website!");
+
+//Ans 3 Alert function doesn,t call because JS is a case sensitive language.
+
+//Ans 4
+alert("Hello, world!");
+
+//Ans 5
+
+
+//Chap 2 (Variables for Strings)
+
+// Ans 1
+var message = "Hello, world!";
+alert(message);
+
+//Ans 2
+var age;       
+age = 25;      
+alert(age);    
+
+//Ans 3
+var greeting = "Good Morning!"; 
+alert(greeting);                 
+
+greeting = "Good Evening!";      
+alert(greeting);          
+
+//Ans 4
+var favoriteColor = "blue";  
+alert("My favorite color is " + favoriteColor);  
+
+//Ans 5
+var age;       
+age = 25;      
+alert(age);    
+
+
+
+
